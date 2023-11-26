1204573833b07b99f3cd36f83e6b60611496022f
Buldakov <abuldakov10@gmail.com>
2023-11-26 19:08:52 +0400

    Add weather and location data

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,11 +1,16 @@
-import { useEffect } from 'react';
+import React, { useEffect } from 'react';
 
 // Files
 import './App.scss';
+import { weatherIconUrl } from './api';
+import { convertTime, convertDate, convertWindDirection } from './index';
 
 // Components
 import useLocationCoord from '../../hooks/useLocationCoord';
 import useGetWeather from '../../hooks/useGetWeather';
+import TemperatureIcon from '../Icons/TemperatureIcon';
+import WindIcon from '../Icons/WindIcon';
+import BarometerIcon from '../Icons/BarometerIcon';
 
 const App = () => {
   const { latitude, longitude, error } = useLocationCoord(); // get location coord
@@ -16,29 +21,78 @@ const App = () => {
   }, [latitude, longitude]);
 
   return (
-    <div className="weather">
-      <div className="weather__day">
-        <div>{latitude && longitude ? `latitude: ${latitude}, longitude: ${longitude}` : `error: ${error}`}</div>
+    <div className="weather-wrapper">
+      <div className="container">
+        <div className="weather">
+          {/*** Location info ***/}
+          <div className="location-info">
+            <div className="location-info__item country">{weatherData?.sys.country},</div>
+            <div className="location-info__item city">{weatherData?.name}</div>
 
-        <button
-          onClick={() => {
-            getWeather(39.907501, 116.397232);
-          }}
-        >
-          other
-        </button>
+            <div className="location-info__item coords">
+              <span>{weatherData?.coord.lat}</span>
+              <span>{weatherData?.coord.lon}</span>
+            </div>
+
+            <div className="location-info__item data-time">{convertTime(weatherData?.dt)}</div>
+
+            <button
+              className="btn"
+              onClick={() => {
+                getWeather(39.907501, 116.397232);
+              }}
+            >
+              Изменить
+            </button>
+          </div>
+
+          {/*** Weather info ***/}
+          <div className="weather__day">
+            <div className="weather__day-item weather__day-date">{convertDate(weatherData?.dt)}</div>
+            <div className="weather__day-item weather__day-desc">{weatherData?.weather[0].description}</div>
+
+            <div className="weather__day-item weather__day-img">
+              <img
+                src={`${weatherIconUrl}/wn/${weatherData?.weather[0].icon}@2x.png`}
+                alt="weather-icon"
+                title={weatherData?.weather[0].description}
+              />
+            </div>
+
+            <div className="weather__day-item weather__day-temp">
+              <div className="temp-icon">
+                <TemperatureIcon />
+              </div>
+
+              <div>За окном {Math.round(weatherData?.main.temp)} &#8451;</div>
+              <div>Ощущается как {Math.round(weatherData?.main.feels_like)} &#8451;</div>
+            </div>
+
+            <div className="weather__day-item weather__day-pressure">
+              <div className="pressure-icon">
+                <BarometerIcon />
+              </div>
+
+              <div>Давление {Math.round(weatherData?.main.pressure / 1.333)} мм. рт. ст.</div>
+            </div>
+
+            <div className="weather__day-item weather__day-wind">
+              <div className="wind-icon">
+                <WindIcon />
+              </div>
+
+              <div>
+                Ветер: {convertWindDirection(weatherData?.wind.deg)} {weatherData?.wind.speed} м/с
+              </div>
+            </div>
+
+            <i className="weather__day-details">Нажмите для просмотра подробностей</i>
+          </div>
+
+          {/*** Weather week list ***/}
+          <div className="weather__week">weather week</div>
+        </div>
       </div>
-
-      <div className="location-info">
-        <div>Текущее местоположение:</div>
-        <div>Страна</div>
-        <div>{weatherData?.name}</div>
-        <div>Координаты</div>
-        <div>Время</div>
-        <button className="btn">Изменить</button>
-      </div>
-
-      <div className="weather__week">weather week</div>
     </div>
   );
 };
---
+++ src/components/App/index.js
@@ -0,0 +1,44 @@
+// Get time
+export const convertTime = (secondsValue) => {
+  const time = new Date(secondsValue * 1000);
+
+  return time.toLocaleTimeString();
+};
+
+// Get date
+export const convertDate = (secondsValue) => {
+  const monthList = [
+    'Января',
+    'Февраля',
+    'Марта',
+    'Апреля',
+    'Мая',
+    'Июня',
+    'Июля',
+    'Августа',
+    'Сентября',
+    'Октября',
+    'Ноября',
+    'Декабря',
+  ];
+  const week = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+  const time = new Date(secondsValue * 1000);
+
+  return `${week[time.getDay()]}, ${time.getDate()} ${monthList[time.getMonth()]}`;
+};
+
+// Get wind direction
+export const convertWindDirection = (windData) => {
+  const windDirectionData = {
+    0: 'южный',
+    90: 'западный',
+    180: 'северный',
+    270: 'восточный',
+  };
+
+  if (windData && Object.keys(windDirectionData).includes(windData.toString())) return windDirectionData[windData];
+  if (windData > 0 && windData < 90) return 'юго-западный';
+  if (windData > 90 && windData < 180) return 'северо-западный';
+  if (windData > 180 && windData < 270) return 'северо-восточный';
+  if (windData > 270 && windData < 360) return 'юго-восточный';
+};
